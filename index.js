94c5536d2e2a3a7580393a3373af1f1d1c3df122
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2023-02-09 13:20:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const {exec} = require("child_process")
-setInterval(()=>{exec("ping https://AlertTemporalMarketing.arch-herobrine.repl.co")},30000)
+setInterval(()=>{exec("curl https://AlertTemporalMarketing.arch-herobrine.repl.co")},30000)
