273fe69320c1feaa81a4481501b859a9c6db09ce
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2023-02-18 16:03:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {exec} = require("child_process");
 setInterval(()=>{exec("curl https://chlorinated-jumpy-hyssop.glitch.me/");
                  exec('curl https://surlfs.f5.si');
                  exec("curl https://verbose-fresh-sphere.glitch.me/");
-                 exec('curl https://fork-translucent-quilt.glitch.me/');
+                 exec('curl https://opposite-soft-citrus.glitch.me/');
                  exec('curl https://voltaic-rowan-fibre.glitch.me/');
                  exec('curl https://cottony-curvy-carpet.glitch.me/');
                  exec('curl https://qawsedrftgyhujikolp.arch-herobrine.repl.co');
